5de6a3cfb16d0c0d0837390e2354448e6ce9cc4c
maru44 <takumaru22244@gmail.com>
2021-04-17 18:49:12 +0900

    GA

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,19 +1,15 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
-interface page_path {
-  [key: string]: string;
-}
-
 interface Window {
-  gtag(type: "config", googleAnalyticsId: string, page_path: page_path);
+  gtag(type: "config", googleAnalyticsId: string, { page_path: string });
   gtag(
     type: "event",
     eventAction: string,
     fieldObject: {
       label: string;
       category: string;
-      value?: number;
+      value?: string;
     }
   );
 }
